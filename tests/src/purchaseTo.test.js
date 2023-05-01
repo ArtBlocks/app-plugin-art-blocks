c4ff7c6e2a66825c70ca91f686cebfa707ef981f
Shantanu Bala <shantanu@sbala.org>
2023-05-01 11:34:52 -0400

    add screens

--- tests/src/purchaseTo.test.js
+++ tests/src/purchaseTo.test.js
@@ -30,12 +30,12 @@ const devices = [
   {
     name: "nanox",
     label: "Nano X",
-    steps: 5, // <= Define the number of steps for this test case and this device
+    steps: 7, // <= Define the number of steps for this test case and this device
   },
   {
     name: "nanosp",
     label: "Nano S+",
-    steps: 5, // <= Define the number of steps for this test case and this device
+    steps: 7, // <= Define the number of steps for this test case and this device
   },
 
 ];
